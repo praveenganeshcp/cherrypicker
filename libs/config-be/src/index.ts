ea6ae52b258c1a1170f9c382f2630e2ceaf8af2f
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-08-17 06:52:51 +0530

    update(github): add github lib, service and models

--- libs/config-be/src/index.ts
+++ libs/config-be/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/config-be.module';
 export * from "./lib/app.config";
 export * from "./lib/db.config";
 export * from "./lib/email.config";
+export * from "./lib/github.config";
--- libs/config-be/src/lib/config-be.module.ts
+++ libs/config-be/src/lib/config-be.module.ts
@@ -4,6 +4,7 @@ import { validateEnvVariables } from './env.validator';
 import { appConfig } from './app.config';
 import { dbConfig } from './db.config';
 import { emailconfig } from './email.config';
+import { githubConfig } from './github.config';
 
 @Module({
   controllers: [],
@@ -14,7 +15,7 @@ import { emailconfig } from './email.config';
       isGlobal: true,
       validate: validateEnvVariables,
       cache: false,
-      load: [appConfig, dbConfig, emailconfig],
+      load: [appConfig, dbConfig, emailconfig, githubConfig],
     })
   ]
 })
