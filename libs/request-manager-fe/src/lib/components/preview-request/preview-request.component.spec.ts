c756741b59807effd90bcece36cd767bbec7cf04
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2024-08-18 18:00:46 +0530

    update(request-manager): build commit info and request preview components

---
+++ libs/request-manager-fe/src/lib/components/preview-request/preview-request.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PreviewRequestComponent } from './preview-request.component';
+
+describe('PreviewRequestComponent', () => {
+  let component: PreviewRequestComponent;
+  let fixture: ComponentFixture<PreviewRequestComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PreviewRequestComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PreviewRequestComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
