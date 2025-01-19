e0d23d82160e631ab44586c9b4615c2a39254ae3
Praveen Kumar <praveenkumar@Praveens-MacBook-Air.local>
2025-01-19 21:59:14 +0530

    update: update api url port

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl: "http://cherrypicker.mvp-by-praveen.click:3000/api",
+  apiUrl: "http://cherrypicker.mvp-by-praveen.click:4000/api",
   clientId: "Ov23liVeF0C6NwnvHwJQ",
   hostAddress: "http://cherrypicker.mvp-by-praveen.click:8082",
 };
